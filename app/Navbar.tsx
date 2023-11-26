ce33ec134f31b6c83bc890fe175b125253a1ee0d
aivargab <aivargab@mail.ru>
2023-11-26 18:50:58 +0500

    1

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -13,25 +13,27 @@ import {
 
 const menuItems = [
   "ППЖ",
-  "ЭКОЛОГИЧЕСКИЙ КОНСАЛТИНГ",
-  "УСЛУГИ",
-  "ПРЕИМУЩЕСТВА",
-  "КЛИЕНТЫ",
-  "КОНТАКТЫ",
+  "Экологический консалтинг",
+  "Услуги",
+  "Преимущество",
+  "Клиеты",
+  "Контакты",
 ];
 
 function CustomNavbar() {
   return (
     <NextUIProvider>
       <div className="fixed m-auto top-5 left-0 right-0 bg-white w-fit p-3 rounded-lg flex gap-3">
-      <p className="font-bold text-xl">Эко-Терра</p>
-        <div className="flex gap-3">
+        <div className="font-bold text-xl mt-[-3px]">Эко-Терра</div>
+        <div className="flex gap-2">
           {menuItems.map((x) => (
             <div>{x}</div>
           ))}
         </div>
         {/**<a href="tel:+ 7 (800) 555-14-08" className="bg-green-400 rounded-xl p-2 text-white">+ 7 (800) 555-14-08</a> */}
-       <a href="tel:+ 7 (800) 555-14-08" className="bg-green-400 text-white font-semibold">+ 7 (800) 555-14-08</a> 
+        <a href="tel:+ 7 (800) 555-14-08" className="font-semibold">
+          + 7 (800) 555-14-08
+        </a>
       </div>
     </NextUIProvider>
   );
