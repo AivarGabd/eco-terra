0baa6a4bbab60c8c41c63c65c29501731f6d76ca
aivargab <aivargab@mail.ru>
2023-11-28 05:36:43 +0500

    1

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 const menuItems = [
   "ППЖ",
   "Экологический консалтинг",
@@ -11,19 +10,32 @@ const menuItems = [
 ];
 
 function CustomNavbar() {
+  const linkClick = (index: number) => {
+    let target: HTMLElement = document.getElementById(`section-${index}`)!;
+    if (!target) return;
+    target.scrollIntoView();
+  };
+
   return (
-    <div className="fixed m-auto top-5 left-0 right-0 bg-white w-fit p-3 rounded-lg flex gap-3">
-        <div className="font-bold text-xl mt-[-3px]">Эко-Терра</div>
-        <div className="flex gap-2">
-          {menuItems.map((x) => (
-            <div key={x}>{x}</div>
-          ))}
-        </div>
-        {/**<a href="tel:+ 7 (800) 555-14-08" className="bg-green-400 rounded-xl p-2 text-white">+ 7 (800) 555-14-08</a> */}
-        <a href="tel:+ 7 (800) 555-14-08" className="font-semibold">
+    <div className="fixed m-auto top-5 left-0 right-0 bg-white w-fit p-3 rounded-lg flex gap-5 z-10 flex-row align-bottom">
+      <div className="font-bold text-xl">Эко-Терра</div>
+      <div className="flex gap-1">
+        {menuItems.map((x, index) => (
+          <button
+            className="hover:bg-gray-100 rounded-lg py-1 px-1.5 transition-all"
+            key={x}
+            onClick={() => linkClick(index)}
+          >
+            {x}
+          </button>
+        ))}
+      </div>
+      <div className="flex h-full align-middle text-center m-auto ">
+        <a href="tel:+ 7 (800) 555-14-08" className="font-semibold mb-auto">
           + 7 (800) 555-14-08
         </a>
       </div>
+    </div>
   );
 }
 
